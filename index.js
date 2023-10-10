841f950bcba7fce655f5d97b303dff0631e83c46
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-10-10 17:16:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here
+class Breakfast {
+    constructor (food, drink) {
+        this.food = food;
+        this.drink = drink;
+
+    }
+}
+let Breakfast2 = new Breakfast('Anjera', 'tea');
+
+
+class Lunch {
+    constructor (salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+let Lunch2 = new Lunch("Broccoli", "Camel", "Vanilla");
+
+class Dinner {
+    #dessert;
+    constructor (salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+
+    }
+}
+let Dinner2 = new Dinner('sukuma', 'mbuzi', 'miwa', "cake");
